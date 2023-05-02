66ebbe65000f125e0e05cf36e2842fb10cbb2d3d
Maxim Kurilyak <mprom13@icloud.com>
2023-05-02 18:50:14 +0600

    dfb

--- src/jvmMain/kotlin/me.ositlar.application/repo/mongoMain.kt
+++ src/jvmMain/kotlin/me.ositlar.application/repo/mongoMain.kt
@@ -4,25 +4,28 @@ import com.mongodb.ExplainVerbosity
 import com.mongodb.client.FindIterable
 import com.mongodb.client.MongoClients
 import me.ositlar.application.parser.Parser
-import org.bson.Document
+import me.ositlar.application.parser.StreamSchedule
 import org.json.JSONObject
+import org.litote.kmongo.getCollection
 import org.litote.kmongo.json
 
-val mongoClient = MongoClients.create("mongodb://localhost:27017")
+val mongoClient = MongoClients.create("mongodb://127.0.0.1:27017")
 val database = mongoClient.getDatabase("db")
 val collection = database.getCollection("test")
+val coll = database.getCollection<StreamSchedule>("schedule")
 
 //var data = collection.find().toString()
 //val mongo = database.getCollection<Pair<String, GroupSchedule>>().apply { drop() }
-var data: String = parse()
+var data = coll.insertOne(parse())
 
-fun parse(): String {
+fun parse(): StreamSchedule {
     val parseStreamSchedule = Parser("65.html")
     val parsed = parseStreamSchedule.parseFile()
-    val map = mapOf(parsed.stream to parsed.groups)
-    //val newCollection = database.getCollection<String>()
-    collection.insertOne(Document(map))
-    return collection.find().toList().json
+//    val map = mapOf(parsed.stream to parsed.groups)
+//    //val newCollection = database.getCollection<String>()
+//    collection.insertOne(Document(map))
+//    return collection.find().toList().json
+    return StreamSchedule(parsed.stream, parsed.groups)
 }
 
 
--- src/jvmMain/kotlin/me.ositlar.application/rest/testRoute.kt
+++ src/jvmMain/kotlin/me.ositlar.application/rest/testRoute.kt
@@ -3,11 +3,11 @@ package me.ositlar.application.rest
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
-import me.ositlar.application.repo.data
+import me.ositlar.application.repo.coll
 
 fun Route.testRoute() {
     route("/MamaMia/"){
-        val asd = data
+        val asd = coll.find()
         get {
             call.respond(asd)
         }
