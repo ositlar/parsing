a4d6c48a482d8fcff5b4cda5e5819e65e0e598c9
domovichenok <vovchikk11111111@mail.ru>
2023-05-17 20:36:39 +0600

    Таблица с цветом

--- src/jsMain/kotlin/groups/CGroupContainer.kt
+++ src/jsMain/kotlin/groups/CGroupContainer.kt
@@ -2,18 +2,23 @@ package component.lesson
 
 import Config
 import common.GroupSchedule
+import csstype.Color
+import csstype.LineStyle.Companion.dashed
+import csstype.LineStyle.Companion.solid
+import csstype.TextAlign
+import emotion.react.css
 import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.json.Json
 import query.QueryError
 import react.FC
 import react.Props
 import react.dom.html.ReactHTML.h1
+import react.dom.html.ReactHTML.label
 import react.dom.html.ReactHTML.table
 import react.dom.html.ReactHTML.tbody
 import react.dom.html.ReactHTML.td
 import react.dom.html.ReactHTML.th
 import react.dom.html.ReactHTML.tr
-import react.useState
 import tanstack.query.core.QueryKey
 import tanstack.react.query.useQuery
 import tools.fetchText
@@ -35,13 +40,22 @@ val CGroupContainer = FC<Props>("GroupContainer") {
     h1 {
         +groupContainer.group
     }
-    //for хуйня, багаеться, не юзнаем
     table {
         tbody {
             val time = listOf("08:00 - 09:30", "09:45 - 11:15", "11:30 - 13:00", "13:55 - 15:25", "15:40 - 17:10")
-            val day = listOf(
-                "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота",
-                "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота"
+            val days = listOf(
+                "Понедельник",
+                "Вторник",
+                "Среда",
+                "Четверг",
+                "Пятница",
+                "Суббота",
+                "Понедельник",
+                "Вторник",
+                "Среда",
+                "Четверг",
+                "Пятница",
+                "Суббота"
             )
             tr {
                 if (groupContainer.group.isNotEmpty()) {
@@ -51,19 +65,48 @@ val CGroupContainer = FC<Props>("GroupContainer") {
                     }
                 }
             }
+            css{
+                borderStyle = dashed
+
+            }
             if (groupContainer.schedule.isNotEmpty()) {
-                day.forEach {
+                days.forEach {
+                    if(count >= 30){
+                        label{
+                            +"НЕЧЕТНАЯ"
+                        }
+                    }
                     tr {
-                        td { +it }
+                        td {
+                            +it
+                            css {
+                                textAlign = TextAlign.left
+                                if (count >= 30) {
+                                    color = Color("Blue")
+                                }
+                            }
+                        }
                         val scheduleArr = groupContainer.schedule
-                        for (i in count..count+4) {
+                        for (i in count..count + 4) {
                             td {
-                                +"${scheduleArr[i].subjectType} ${scheduleArr[i].subject} ${scheduleArr[i].teacher} ${scheduleArr[i].place}  "
+
+                                +"${scheduleArr[i].subjectType} "
+                                +"${scheduleArr[i].subject} "
+                                +"${scheduleArr[i].teacher} "
+                                +"${scheduleArr[i].place}  "
+                                css {
+                                    textAlign = TextAlign.center
+                                    if (count >= 30) {
+                                        color = Color("Blue")
+
+                                    }
+                                }
                             }
                         }
                         count += 5
                     }
                 }
+
             }
         }
     }
