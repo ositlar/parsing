c8d0df4a33a67205cef9f51c3b8773d8d90eb54c
domovichenok <vovchikk11111111@mail.ru>
2023-05-21 20:17:47 +0600

    Кафедральное расписание, дни, время

--- src/jsMain/kotlin/Cathedra/CCatherda.kt
+++ src/jsMain/kotlin/Cathedra/CCatherda.kt
@@ -7,6 +7,13 @@ import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.json.Json
 import query.QueryError
 import react.FC
+import react.dom.html.ReactHTML.div
+import react.dom.html.ReactHTML.h1
+import react.dom.html.ReactHTML.table
+import react.dom.html.ReactHTML.tbody
+import react.dom.html.ReactHTML.td
+import react.dom.html.ReactHTML.th
+import react.dom.html.ReactHTML.tr
 import tanstack.query.core.QueryKey
 import tanstack.react.query.useQuery
 import tools.fetchText
@@ -25,4 +32,43 @@ val CCathedra = FC<GroupProps>("Cathedra") { props ->
         emptyMap()
     }
 
+    val time = listOf("08:00 - 09:30", "09:45 - 11:15", "11:30 - 13:00", "13:55 - 15:25", "15:40 - 17:10")
+    val days = listOf(
+        "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", " ",
+        "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота",
+    )
+    h1 {
+        +"Расписание кафедры АиСУ"
+    }
+    div {
+        table {
+            tbody {
+                tr {
+                    days.forEach { day ->
+                        th {
+                            +day
+                        }
+                    }
+                }
+                tr {
+                    days.forEachIndexed { index, _ ->
+                        if (index != 6) {
+                            td {
+                                time.forEach { time ->
+                                    td {
+                                        +time
+                                    }
+                                }
+                            }
+                        }
+                        else{
+                            td{
+                                +"ФИО Преподователя"
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
