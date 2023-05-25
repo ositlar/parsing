d993f1f7ee366cf2cab423326a774895bc9b0a33
domovichenok <vovchikk11111111@mail.ru>
2023-05-26 01:39:00 +0600

    ГОТОВАЯ

--- src/jsMain/kotlin/groups/CSetGroup.kt
+++ src/jsMain/kotlin/groups/CSetGroup.kt
@@ -1,5 +1,6 @@
 package groups
 
+
 import Config
 import common.GroupSchedule
 import common.SubjectInGroup
@@ -13,22 +14,25 @@ import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 import react.FC
 import react.Props
-import react.dom.html.ReactHTML
 import react.dom.html.ReactHTML.div
 import react.dom.html.ReactHTML.h1
-import react.dom.html.ReactHTML.input
+import react.dom.html.ReactHTML.label
+import react.dom.html.ReactHTML.p
+import react.dom.html.ReactHTML.span
 import react.dom.html.ReactHTML.table
 import react.dom.html.ReactHTML.tbody
 import react.dom.html.ReactHTML.td
 import react.dom.html.ReactHTML.th
 import react.dom.html.ReactHTML.tr
 import react.router.dom.Link
+import react.useState
 import tanstack.query.core.QueryKey
 import tanstack.react.query.useMutation
 import tanstack.react.query.useQueryClient
 import tools.HTTPResult
 import tools.fetch
 import kotlin.js.json
+
 
 external interface SetGroupProps : Props {
     var groupSchedule: GroupSchedule
@@ -40,8 +44,7 @@ val CSetGroup = FC<SetGroupProps>("SetGroup") { props ->
     var count = 0
     val selectQueryKey = arrayOf("Group").unsafeCast<QueryKey>()
 
-    val updateMutation = useMutation<HTTPResult, Any, GroupSchedule, Any>(
-        mutationFn = { item: GroupSchedule ->
+    val updateMutation = useMutation<HTTPResult, Any, GroupSchedule, Any>(mutationFn = { item: GroupSchedule ->
         fetch(Config.editSchedulePath + "Update", jso {
             method = "POST"
             headers = json(
@@ -55,33 +58,36 @@ val CSetGroup = FC<SetGroupProps>("SetGroup") { props ->
         }
     })
 
-    val subjectInGroup: MutableList<SubjectInGroup> = mutableListOf()
+    val groupSchedule= props.groupSchedule
+
+
+    val (draggedCell, setDraggedCell) = useState<SubjectInGroup>()
 
     h1 {
         className = ClassName("nameGroup")
         +"Редактирование расписания группы:"
         +props.groupSchedule.group
     }
-
-    ReactHTML.p {
+    p {
         className = ClassName("btnChange")
         Link {
-            ReactHTML.span {
+            span {
                 +"→"
             }
             +"Применить"
             onClick = {
-                val groupSchedule = GroupSchedule(props.groupSchedule.group, subjectInGroup.toMutableList())
                 updateMutation.mutateAsync(groupSchedule, null)
                 props.setButtonState(true)
             }
-            ReactHTML.span {
+            span {
                 +"←"
             }
         }
     }
+
     div {
         table {
+            className = ClassName("table")
             tbody {
                 val time = listOf("08:00 - 09:30", "09:45 - 11:15", "11:30 - 13:00", "13:55 - 15:25", "15:40 - 17:10")
                 val days = listOf(
@@ -91,6 +97,7 @@ val CSetGroup = FC<SetGroupProps>("SetGroup") { props ->
                 tr {
                     if (props.groupSchedule.group.isNotEmpty()) {
                         th { +"Время:" }
+                        className = ClassName("th")
                         time.forEach {
                             th { +it }
                         }
@@ -103,6 +110,7 @@ val CSetGroup = FC<SetGroupProps>("SetGroup") { props ->
                     days.forEachIndexed { index, it ->
                         tr {
                             td {
+                                className = ClassName("td")
                                 +it
                                 css {
                                     textAlign = TextAlign.left
@@ -111,96 +119,69 @@ val CSetGroup = FC<SetGroupProps>("SetGroup") { props ->
                                     }
                                 }
                             }
-                            val scheduleArr = props.groupSchedule.schedule
+                            val scheduleArr = groupSchedule.schedule
                             for (i in count..count + 4) {
                                 td {
+                                    className = ClassName("td")
+                                    draggable = true
+                                    onDragStart = {
+
+                                        setDraggedCell(
+                                            SubjectInGroup(
+                                                props.groupSchedule.group,
+                                                index,
+                                                i,
+                                                scheduleArr[i].subjectType!!,
+                                                scheduleArr[i].subject,
+                                                scheduleArr[i].teacher,
+                                                scheduleArr[i].place
+                                            )
+                                        )
+                                    }
+                                    onDragOver = { event ->
+                                        event.preventDefault()
+                                    }
+                                    onDrop = {
+                                        it.preventDefault()
+
+                                        val temp = groupSchedule.schedule[i]
+                                        groupSchedule.schedule[i] = SubjectInGroup(
+                                            props.groupSchedule.group,
+                                            index,
+                                            i,
+                                            draggedCell!!.subjectType,
+                                            draggedCell.subject,
+                                            draggedCell.teacher,
+                                            draggedCell.place
+
+                                        )
+                                        groupSchedule.schedule[draggedCell.time] =
+                                            SubjectInGroup(
+                                                draggedCell.group,
+                                                draggedCell.dayOfWeek,
+                                                draggedCell.time,
+                                                temp.subjectType,
+                                                temp.subject,
+                                                temp.teacher,
+                                                temp.place
+                                            )
+
+                                        setDraggedCell(temp)
+
+                                    }
                                     if (scheduleArr[i].subject != "_") {
-                                        input {
-                                            defaultValue = scheduleArr[i].subjectType
-                                            onChange = {
-                                                if (it.target.value.isNotEmpty())
-                                                    subjectInGroup[i].subjectType = it.target.value
-                                                else
-                                                    subjectInGroup[i].subjectType ="_"
-                                            }
-                                        }
-                                        input {
-                                            defaultValue = scheduleArr[i].subject
-                                            onChange = {
-                                                if (it.target.value.isNotEmpty())
-                                                    subjectInGroup[i].subject = it.target.value
-                                                else
-                                                    subjectInGroup[i].subject = "_"
-                                            }
-                                        }
-                                        input {
-                                            defaultValue = scheduleArr[i].teacher
-                                            onChange = {
-                                                if (it.target.value.isNotEmpty())
-                                                    subjectInGroup[i].teacher = it.target.value
-                                                else
-                                                    subjectInGroup[i].teacher = "_"
-                                            }
-                                        }
-                                        input {
-                                            defaultValue = scheduleArr[i].place.toString()
-                                            onChange = {
-                                                if (it.target.value.isNotEmpty())
-                                                    subjectInGroup[i].place = it.target.value
-                                                else
-                                                    subjectInGroup[i].place = "_"
-                                            }
+                                        label {
+                                            +"${scheduleArr[i].subjectType!!} "
+                                            +"${scheduleArr[i].subject} "
+                                            +"${scheduleArr[i].teacher} "
+                                            +"${scheduleArr[i].place}"
                                         }
                                     } else {
-                                        input {
-                                            defaultValue = " - "
-                                            onChange = {
-                                                if (it.target.value.isNotEmpty())
-                                                    subjectInGroup[i].subjectType = it.target.value
-                                                else
-                                                    subjectInGroup[i].subjectType = "_"
-                                            }
-                                        }
-                                        input {
-                                            defaultValue ="_"
-                                            onChange = {
-                                                if (it.target.value.isNotEmpty())
-                                                    subjectInGroup[i].subject = it.target.value
-                                                else
-                                                    subjectInGroup[i].subject ="_"
-                                            }
-                                        }
-                                        input {
-                                            defaultValue = "_"
-                                            onChange = {
-                                                if (it.target.value.isNotEmpty())
-                                                    subjectInGroup[i].teacher = it.target.value
-                                                else
-                                                    subjectInGroup[i].teacher = "_"
-                                            }
-                                        }
-                                        input {
-                                            defaultValue = "_"
-                                            onChange = {
-                                                if (it.target.value.isNotEmpty())
-                                                    subjectInGroup[i].place = it.target.value
-                                                else
-                                                    subjectInGroup[i].place = "_"
-                                            }
+                                        label {
+                                            +" - "
                                         }
                                     }
                                 }
-                                subjectInGroup.add(
-                                    SubjectInGroup(
-                                        props.groupSchedule.group,
-                                        index,
-                                        i,
-                                        scheduleArr[i].subjectType,
-                                        scheduleArr[i].subject,
-                                        scheduleArr[i].teacher,
-                                        scheduleArr[i].place
-                                    )
-                                )
                             }
                         }
                         count += 5
